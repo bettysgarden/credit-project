ee7eba0984e21910234f2f4fc12d6c4dea576c82
bettysgarden <kolesnikova268@mail.ru>
2024-10-07 15:21:35 +0300

    конфигурация .gitignore

--- conveyor/src/main/java/ru/test/conveyor/ConveyorApplication.java
+++ conveyor/src/main/java/ru/test/conveyor/ConveyorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConveyorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConveyorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConveyorApplication.class, args);
+    }
 
 }
--- conveyor/src/test/java/ru/test/conveyor/ConveyorApplicationTests.java
+++ conveyor/src/test/java/ru/test/conveyor/ConveyorApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConveyorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
