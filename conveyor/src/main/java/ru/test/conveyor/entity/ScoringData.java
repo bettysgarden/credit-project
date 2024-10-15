582cc1e976413d6ae15e0c23f8cef3de26b8f30d
bettysgarden <ekolesnikova@inobitec.com>
2024-10-15 16:59:14 +0300

    - добавлены entity сущности

---
+++ conveyor/src/main/java/ru/test/conveyor/entity/ScoringData.java
@@ -0,0 +1,33 @@
+package ru.test.conveyor.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.test.conveyor.enums.Gender;
+import ru.test.conveyor.enums.MaritalStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScoringData {
+    private BigDecimal amount;
+    private Integer term;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private LocalDate birthDate;
+    private String passportSeries;
+    private String passportNumber;
+    private LocalDate passportIssueDate;
+    private String passportIssueBranch;
+    private MaritalStatus maritalStatus;
+    private Integer dependentAmount;
+    private Employment employment;
+    private String account;
+    private Boolean isInsuranceEnabled;
+    private Boolean isSalaryClient;
+}
+
